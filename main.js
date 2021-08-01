5e4ccf2b44726a1989560295700622e67f68682d
rashel68 <rashel341068@gmail.com>
2021-08-02 01:12:29 +0600

    second-practice-js

---
+++ main.js
@@ -0,0 +1,57 @@
+var fruitsName = ['Orange','Tomato', 'Mango', 'Lichi', 'Apple', 'Dragon'];
+
+
+// var fruitsPosition =fruitsName.indexOf('Apple');
+// console.log(fruitsPosition);
+
+// var fruitsNumber = fruitsName[5];
+// console.log(fruitsNumber);
+
+// var fruitsQuantity = fruitsName.length;
+// console.log(fruitsQuantity);
+
+// fruitsName.push('Peyara');
+// console.log(fruitsName);
+// var newFruits = fruitsName.push('Ankur');
+// console.log(newFruits);
+
+// fruitsName.pop();
+// var removeItems = fruitsName.pop();
+// console.log(fruitsName);
+// console.log('Removed Item:' + removeItems);
+
+// fruitsName.shift();
+// console.log(fruitsName);
+// var removeFirst = fruitsName.shift();
+// console.log(fruitsName);
+// console.log('This Fruits Removed From First Position : ' + removeFirst);
+
+// fruitsName.unshift('Green Apple');
+// console.log(fruitsName);
+// var addFirst = fruitsName.unshift('Green Apple');
+// console.log(fruitsName);
+// console.log('This fruits newly added from first side ' + addFirst);
+
+
+// if(5 < fruitsName.length){
+//     console.log('You can add some fruits');
+// }else{
+//     console.log('Sorry you can not add fruits');
+// }
+
+
+// if(4 < fruitsName.length){
+//     fruitsName.push('Boroi');
+//     console.log(fruitsName);
+// }else{
+//     console.log("Sorry! You can not add fruits");
+// }
+
+
+// if(fruitsName.length > 10){
+//     var removedFruits = fruitsName.pop();
+//     console.log(fruitsName);
+//     console.log('this fruits removed: ' + removedFruits);
+// }else{
+//     console.log("You don't need to remove fruits");
+// }
