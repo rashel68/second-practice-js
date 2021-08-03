5bf70d724d2e4fe3a815afc923dba8d7df187140
rashel68 <rashel341068@gmail.com>
2021-08-03 11:09:28 +0600

    array-notes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var fruitsName = ['Orange','Tomato', 'Mango', 'Lichi', 'Apple', 'Dragon'];
+var newItem = "Komola";
 
 
 // var fruitsPosition =fruitsName.indexOf('Apple');
@@ -7,25 +8,34 @@ var fruitsName = ['Orange','Tomato', 'Mango', 'Lichi', 'Apple', 'Dragon'];
 // var fruitsNumber = fruitsName[5];
 // console.log(fruitsNumber);
 
+// array length 
 // var fruitsQuantity = fruitsName.length;
 // console.log(fruitsQuantity);
 
-// fruitsName.push('Peyara');
+// array element replace 
+// fruitsName[0]='Komola';
 // console.log(fruitsName);
-// var newFruits = fruitsName.push('Ankur');
-// console.log(newFruits);
 
+// array element push/add from right side 
+// fruitsName.push('Peyara');
+// fruitsName.push(newItem);
+// console.log(fruitsName);
+// console.log(newItem);
+
+// array element pop/remove from right side 
 // fruitsName.pop();
 // var removeItems = fruitsName.pop();
 // console.log(fruitsName);
 // console.log('Removed Item:' + removeItems);
 
+// array element shift/remove from left side 
 // fruitsName.shift();
 // console.log(fruitsName);
 // var removeFirst = fruitsName.shift();
 // console.log(fruitsName);
 // console.log('This Fruits Removed From First Position : ' + removeFirst);
 
+// array element unshift/add from left side 
 // fruitsName.unshift('Green Apple');
 // console.log(fruitsName);
 // var addFirst = fruitsName.unshift('Green Apple');
@@ -54,4 +64,41 @@ var fruitsName = ['Orange','Tomato', 'Mango', 'Lichi', 'Apple', 'Dragon'];
 //     console.log('this fruits removed: ' + removedFruits);
 // }else{
 //     console.log("You don't need to remove fruits");
+// }
+
+
+// while loop
+var num = 1;
+
+// while(num<=20){
+//     console.log(num);
+//     num++;
+// }
+
+// // even number 
+// while(num<20){
+//     console.log(num);
+//     num+=2;
+// }
+
+// odd number 
+// while(num<20){
+//     console.log(num);
+//     num+=2;
+// }
+
+// // For Loop 
+// for(var number = 1; number<=20; number++){
+//     console.log(number);
+// }
+
+// array loop 
+// for( var i = 0; i<fruitsName.length; i++){
+//     var fruitsList = fruitsName[i];
+//     console.log(fruitsList);
+// }
+
+// for(var i = 0; i<fruitsName.length; i++){
+//     var fruits = fruitsName[i];
+//     console.log(i + '. ' + fruits);
 // }
